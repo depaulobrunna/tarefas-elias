421a0e0cd4dfcf1e5f266d4c3331cf8894f48927
Brunna <depaulobr@gmail.com>
2018-05-14 11:04:52 -0300

    registrador durante a conversao esta e em ponto flutuante

--- ADC/ADC.c
+++ ADC/ADC.c
@@ -2,37 +2,51 @@
 
 uint8_t i,count,timeout = 100,data;
 uint32_t reset;
+float volts = 0.0f;
+volatile uint32_t tick = 0;
+
+ADC_TypeDef *adc = ADC3;
 
 int main(void)
 {
+	SysTick_Config(SystemCoreClock/1000);
 	//GPIO CFG(ANALOG MODE)
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-	GPIOA->MODER |= GPIO_MODER_MODE0_0|GPIO_MODER_MODE0_1;//PA 0
+	GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODE0) | (3 << GPIO_MODER_MODE0_Pos);//PA 0 analog mode
 	
-	RCC->APB2ENR |= RCC_APB2ENR_ADC2EN;
-	ADC2->CR2 = (ADC2->CR2 & ~ADC_CR2_ADON)|(0 << ADC_CR2_ADON);
+	
+	RCC->APB2ENR |= RCC_APB2ENR_ADC3EN;
+	adc->CR2 = (adc->CR2 & ~ADC_CR2_ADON)|(0 << ADC_CR2_ADON);
     
-	ADC2->CR1 = (ADC2->CR1 & ~(ADC_CR1_RES|ADC_CR1_AWDEN))|(1 << ADC_CR1_RES_Pos);
-	ADC2->SQR1 = (ADC2->SQR1 & ~ADC_SQR1_L)|(0 << ADC_SQR1_L_Pos);
-	ADC2->SQR3 = (ADC2->SQR3 & ~ADC_SQR3_SQ1)|(1 << ADC_SQR3_SQ1_Pos);
-	ADC2->SMPR2 = (ADC2->SMPR2 & ~ADC_SMPR2_SMP1)|(8 << ADC_SMPR2_SMP1_Pos);
-	ADC2->CR2 = (ADC2->CR2 & ~(ADC_CR2_CONT|ADC_CR2_ADON))|((0 << ADC_CR2_CONT_Pos)|(1 << ADC_CR2_ADON_Pos));
+	adc->CR1 = (adc->CR1 & ~(ADC_CR1_RES|ADC_CR1_AWDEN))|(2 << ADC_CR1_RES_Pos);
+	adc->SQR1 = (adc->SQR1 & ~ADC_SQR1_L)|(0 << ADC_SQR1_L_Pos);
+	adc->SQR3 = (adc->SQR3 & ~ADC_SQR3_SQ1)|(1 << ADC_SQR3_SQ1_Pos);
+	adc->SMPR2 = (adc->SMPR2 & ~ADC_SMPR2_SMP1)|(8 << ADC_SMPR2_SMP1_Pos);
+	adc->CR2 = (adc->CR2 & ~(ADC_CR2_CONT|ADC_CR2_ADON))|((1 << ADC_CR2_CONT_Pos)|(1 << ADC_CR2_ADON_Pos));
 	
 	
 	while(1)
 	{
-    #if 1	
-				ADC2->DR = 0x00;
-        while(!(ADC2->SR & ADC_SR_STRT))
-        {
-            ADC2->CR2 = (ADC2->CR2 & ~ADC_CR2_SWSTART)|(1 << ADC_CR2_SWSTART_Pos);
-        }
-				
-				//while(((ADC2->SR & ADC_SR_EOC) == reset) && (count < timeout)) count++;
-        while((ADC->CSR & ADC_CSR_EOC2) != ADC_CSR_EOC2 );//ESPERA A CONVERSAO TERMINAR
-        ADC->CSR &= ~ADC_CSR_EOC2;
-				//for (i=0; i < 100 ;i ++);
-				data = ADC2->DR;
-    #endif
+#if 1	
+		while(!(adc->SR & ADC_SR_STRT))
+		{
+				adc->CR2 = (adc->CR2 & ~ADC_CR2_SWSTART)|(1 << ADC_CR2_SWSTART_Pos);
+		}
+
+		//while(((ADC2->SR & ADC_SR_EOC) == reset) && (count < timeout)) count++;
+		while(!(adc->SR & ADC_SR_EOC));//ESPERA A CONVERSAO TERMINAR
+//		adc->SR &= ~ADC_SR_EOC;
+
+		data = adc->DR;
+		tick = 0;
+		while(tick < 1000);
+		volts = (float)((float)((uint8_t)adc->DR)/(float)255 * 3.3f);
+			
+#endif
 	}
+}
+
+void SysTick_Handler(void)
+{
+	tick++;
 }
