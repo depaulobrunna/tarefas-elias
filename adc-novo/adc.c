a2970b86a5edc41f152685bcc3d41d9e93958304
Brunna <depaulobr@gmail.com>
2018-05-21 19:45:46 -0300

    interrupçao adc travando o codigo

--- adc-novo/adc.c
+++ adc-novo/adc.c
@@ -2,20 +2,21 @@
 
 #define MILISECONDS(x)	(uint32_t)(x)
 #define SECONDS(x)			(uint32_t)(x * MILISECONDS(100))
+#define NUM_AMOST				(uint32_t)(10)
 
 #define SALVA_DADO		
 #define INVERTE_LED			GPIOD->ODR ^= GPIO_ODR_OD12;
 #define USE_SYSTICK
 #ifdef USE_SYSTICK
-	#define SYSTICK_TIME		MILISECONDS(100)
+	#define SYSTICK_TIME		MILISECONDS(10)
 #endif
 
-uint32_t i, temp;
+
 float volts = 0.0f;
 volatile uint32_t tick = 0;
-uint8_t data[100];
-uint8_t *point = &data[0];
-uint8_t dado;
+volatile uint32_t i=0;
+volatile uint8_t data[NUM_AMOST];
+volatile uint8_t *point = &data[0];
 
 void SysTick_Handler(void);
 void ADC_IRQHandler(void);
@@ -41,11 +42,12 @@ int main(void)
 	RCC->APB2ENR |= RCC_APB2ENR_ADC3EN;
 	adc->CR2 = (adc->CR2 & ~ADC_CR2_ADON)|(0 << ADC_CR2_ADON);
     
-	adc->CR1 = (adc->CR1 & ~(ADC_CR1_RES|ADC_CR1_AWDEN))|(2 << ADC_CR1_RES_Pos)|(1 << ADC_CR1_EOCIE_Pos);// ADC 8BITS & INTERRUPT DISABLE
+	adc->CR1 = (adc->CR1 & ~(ADC_CR1_RES|ADC_CR1_AWDEN))|(2 << ADC_CR1_RES_Pos)|(0 << ADC_CR1_EOCIE_Pos);// ADC 8BITS & INTERRUPT ABLE
 	adc->SQR1 = (adc->SQR1 & ~ADC_SQR1_L)|(0 << ADC_SQR1_L_Pos);//1 CONV
 	adc->SQR3 = (adc->SQR3 & ~ADC_SQR3_SQ1)|(0 << ADC_SQR3_SQ1_Pos);//CHANEL 0
 	adc->SMPR2 = (adc->SMPR2 & ~ADC_SMPR2_SMP1)|(8 << ADC_SMPR2_SMP1_Pos);//SAMMPLING TIME
-	adc->CR2 = (adc->CR2 & ~(ADC_CR2_CONT|ADC_CR2_ADON))|((1 << ADC_CR2_CONT_Pos)|(1 << ADC_CR2_ADON_Pos));//continuous mode on & TURN ON ADC
+	adc->CR2 = (adc->CR2 & ~(ADC_CR2_CONT|ADC_CR2_ADON))|((1 << ADC_CR2_CONT_Pos)|(1 << ADC_CR2_ADON_Pos)
+						 |(1 << ADC_CR2_EXTEN_Pos)|(3 << ADC_CR2_EXTSEL_Pos));//continuous mode on & TURN ON ADC & EXTEN RISING EDGE &EXSEL TIMER CANAL 2 EVENT
 	
 	NVIC_EnableIRQ(ADC_IRQn);
 #endif
@@ -60,23 +62,29 @@ int main(void)
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 	
 	TIM2->PSC = 1000 - 1;//VALOR PELO QUAL O CLOCK INICIAL É DIVIDIDO
-	TIM2->ARR = 16000 - 1;
+	TIM2->ARR = 16 - 1;
 	TIM2->CR1 |= TIM_CR1_CEN;
 	
 	TIM2->CCMR1 = (TIM2->CCMR1 & ~TIM_CCMR1_OC2M_Pos)| (6 << TIM_CCMR1_OC2M_Pos);
 	TIM2->CR1 |= TIM_CR1_ARPE;
-	TIM2->CCR2 = 8000 - 1;//tempo alto
+	TIM2->CCR2 = 8 - 1;//tempo alto
 	TIM2->CCER = (TIM2->CCER & ~TIM_CCER_CC2E)| (1 << TIM_CCER_CC2E_Pos);
 	
 	while(1)
 	{
+	#if 0
 		//volts = (float)((float)((uint8_t)adc->DR)/(float)255 * 3.0f);
-		while(!(TIM2->SR & TIM_SR_UIF));
+		
 		//adc->CR2 = (adc->CR2 & ~ADC_CR2_SWSTART)|(1 << ADC_CR2_SWSTART_Pos);
-		TIM2->SR &= ~(TIM_SR_UIF);
+	
 		INVERTE_LED
 		//dado = adc->DR;
+	#endif
 		
+		while(i < NUM_AMOST);
+		TIM2->CR1 &= ~TIM_CR1_CEN;
+		adc->CR2 = (adc->CR2 & ~(ADC_CR2_ADON))|(0 << ADC_CR2_ADON_Pos);
+		while(1);
 		
 	#if 0
 		tick = 0;
@@ -93,6 +101,6 @@ void SysTick_Handler(void)
 
 void ADC_IRQHandler(void)
 {
-	INVERTE_LED
+	data[i++] = adc->DR;
 	
 }
