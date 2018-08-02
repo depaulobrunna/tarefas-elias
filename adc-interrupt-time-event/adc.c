f354319daf09a56ba17f22d3bb274028fe91631e
depaulobrunna <brunna@ifce.edu.br>
2018-08-02 16:23:44 -0300

    dma n funcionando, timmer de strt doad em 2KHz e srtr em 1KHz

--- adc-interrupt-time-event/adc.c
+++ adc-interrupt-time-event/adc.c
@@ -14,7 +14,7 @@ GPIO_TypeDef *gpio = GPIOA;
 int main(void)
 {
 	//led cfg
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+ 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
 	GPIOD->MODER |= GPIO_MODER_MODER12_0|GPIO_MODER_MODER13_0|GPIO_MODER_MODER14_0; //12gr 13og
 	GPIOD->ODR &= ~(GPIO_ODR_OD12|GPIO_ODR_OD13|GPIO_ODR_OD14);
 # if 1
@@ -53,8 +53,8 @@ int main(void)
 							(1 << ADC_CR2_EXTEN_Pos)| //EXTEN RISING EDGE
 							(3 << ADC_CR2_EXTSEL_Pos)| //EXSEL TIMER CANAL 2 EVENT
 							(1 << ADC_CR2_EOCS_Pos)| // End of conversion selection = the EOC bit is set at the end of each regular conversion
-							(1 << ADC_CR2_DMA_Pos)|//Direct memory access mode DISABLE
-							(1 << ADC_CR2_DDS_Pos);//DMA requests are issued as long as data are converted and DMA=1  DISABLE
+							(1 << ADC_CR2_DMA_Pos)|//Direct memory access mode ABLE
+							(0 << ADC_CR2_DDS_Pos);//DMA requests are issued as long as data are converted and DMA=1  ABLE
 	NVIC_EnableIRQ(ADC_IRQn);
 
 	//confg pin timmer 
@@ -68,11 +68,11 @@ int main(void)
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 	
 	TIM2->PSC = 1000 - 1;//VALOR PELO QUAL O CLOCK INICIAL É DIVIDIDO
-	TIM2->ARR = 16 - 1;
+	TIM2->ARR = 8 - 1;
 	
 	TIM2->CCMR1 = (TIM2->CCMR1 & ~TIM_CCMR1_OC2M_Pos)| (6 << TIM_CCMR1_OC2M_Pos);
 	TIM2->CR1 |= TIM_CR1_ARPE;
-	TIM2->CCR2 = 8 - 1;//tempo alto
+	TIM2->CCR2 = 4 - 1;//tempo alto
 	TIM2->CCER = (TIM2->CCER & ~TIM_CCER_CC2E)| (1 << TIM_CCER_CC2E_Pos);
 	
 	adc->CR2 |= ADC_CR2_ADON;
@@ -81,12 +81,13 @@ int main(void)
 	
 	while(1)
 	{
-	#if 0
+	
+		
+	#if 1
 		i = adc->DR;
-		while(!(ADC3->SR & ADC_SR_EOC))
-		{ 
-			GPIOD->ODR ^= GPIO_ODR_OD14;//red
-		}
+		while(!(adc->SR & ADC_SR_STRT));
+		
+		GPIOD->ODR ^= GPIO_ODR_OD14;//red
 	#endif
 	}
 }
@@ -94,11 +95,13 @@ int main(void)
 void ADC_IRQHandler(void)
 {
 	//An interrupt can be produced on the end of conversion adc
-	GPIOD->ODR ^= GPIO_ODR_OD13;//og
+	
+	GPIOD->ODR ^= GPIO_ODR_OD13;//og osc=AZUL
+	adc->SR &= ~ADC_SR_STRT;
 	
 }
 void DMA2_Stream0_IRQHandler(void)
 {
 	//Transfer complete dma
-	GPIOD->ODR ^= GPIO_ODR_OD12;//gr
+	GPIOD->ODR ^= GPIO_ODR_OD12;//gr osc=ROXO
 }
